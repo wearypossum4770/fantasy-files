ae9eab6d6899c9dd83d352c9ff5a7f54e8896777
Dev Steve <wearypossum4770@yahoo.com>
2024-06-03 15:32:25 -0400

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,40 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import { getUser } from "~/session.server";
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return json({ user: await getUser(request) });
+};
+
+export default function App() {
+  return (
+    <html lang="en" >
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body >
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/_index.tsx
@@ -0,0 +1,15 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+    <main>
+      <h1>Comming Soon</h1>
+    </main>
+  );
+}
